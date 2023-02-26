129a82e0919646361fa236cbab9456e1e3e808f5
hazem008 <hazem.kharroubi@esprit.tn>
2023-02-26 21:41:25 +0100

    Add Class CustomAuthentificationFilter and make a littel code in SecurityConfig Class

--- src/main/java/pidev/tn/aurora/security/SecurityConfig.java
+++ src/main/java/pidev/tn/aurora/security/SecurityConfig.java
@@ -1,13 +1,17 @@
 package pidev.tn.aurora.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import pidev.tn.aurora.filter.CustomAuthentificationFilter;
 import pidev.tn.aurora.services.UserApp.IServiceUser;
 
 @Configuration
@@ -23,6 +27,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.addFilter(new CustomAuthentificationFilter(authenticationManagerBean()));
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean()throws Exception{
+        return super.authenticationManagerBean();
     }
 }
