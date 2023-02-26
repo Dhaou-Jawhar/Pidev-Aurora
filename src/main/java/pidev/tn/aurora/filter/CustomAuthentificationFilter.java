8af847be8e4d75de86534d60a6e883fcb82ea542
hazem008 <hazem.kharroubi@esprit.tn>
2023-02-26 23:22:16 +0100

    Add dependency JWT and add code to successfulAuthentication() method

--- src/main/java/pidev/tn/aurora/filter/CustomAuthentificationFilter.java
+++ src/main/java/pidev/tn/aurora/filter/CustomAuthentificationFilter.java
@@ -1,10 +1,14 @@
 package pidev.tn.aurora.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -12,6 +16,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class CustomAuthentificationFilter extends UsernamePasswordAuthenticationFilter {
@@ -29,7 +35,21 @@ public class CustomAuthentificationFilter extends UsernamePasswordAuthentication
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        User user = (User)authentication.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+        String access_token= JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()+ 10 * 60 * 1000))
+                .withIssuer(request.getRequestURI().toString())
+                .withClaim("role",user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(algorithm);
+        String refresh_token= JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()+ 30 * 60 * 1000))
+                .withIssuer(request.getRequestURI().toString())
+                .sign(algorithm);
+        response.setHeader("access_token",access_token);
+        response.setHeader("refresh_token",refresh_token);
     }
 }
