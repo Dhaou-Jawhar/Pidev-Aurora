977bebb97551015b857f6551e9585137e0123ea9
islem-yacoubi <islem.yacoubi@esprit.tn>
2023-02-27 13:53:49 +0100

    modif ba3d userapp

--- src/main/java/pidev/tn/aurora/services/Users/SeviceUser.java
+++ src/main/java/pidev/tn/aurora/services/Users/SeviceUser.java
@@ -3,7 +3,7 @@ package pidev.tn.aurora.services.Users;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import pidev.tn.aurora.entities.User.Users;
+import pidev.tn.aurora.entities.User.UserApp;
 import pidev.tn.aurora.repository.Users.UsersRepository;
 
 import java.util.List;
@@ -15,18 +15,18 @@ public class SeviceUser implements IServiceUsers{
     @Autowired
     public UsersRepository usersRepository;
     @Override
-    public Users addOrUpdateUser(Users users) {
+    public UserApp addOrUpdateUser(UserApp userApp) {
 
-        return usersRepository.save(users);
+        return usersRepository.save(userApp);
     }
 
-    public List<Users>  GetAllUser(){
-       List<Users> listUsers= usersRepository.findAll();
+    public List<UserApp>  GetAllUser(){
+       List<UserApp> listUsers= usersRepository.findAll();
        return  listUsers;
     }
 
     @Override
-    public Users GetUser(Integer id) {
+    public UserApp GetUser(Integer id) {
         return usersRepository.findById(id).orElse(null);
     }
 
