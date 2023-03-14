7470531e161b1011a8b8e7291cc6d5df48763d7a
hanen hamouda <hanen.hammouda@esprit.tn>
2023-03-14 11:16:07 +0100

    rating done

--- src/main/java/pidev/tn/aurora/repository/Event/RatingRepository.java
+++ src/main/java/pidev/tn/aurora/repository/Event/RatingRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.repository.query.Param;
 import pidev.tn.aurora.entities.Event.Rating;
 
 public interface RatingRepository extends JpaRepository<Rating, Integer> {
+    @Query("select AVG(cr.note) from Events c inner join c.ratings cr where c.id= :id")
+    float NbRating(Integer id);
+
 }
